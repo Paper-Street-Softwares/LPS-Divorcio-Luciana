db394a9c306beb75c785cec4ae46e658d0452e35
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-02-01 16:11:03 -0300

    fix: Limpa codigo

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,16 +3,12 @@ import { lazy, Suspense } from 'react'
 // CRÍTICO — carregamento imediato (LCP)
 import NavbarNovaTemplate from '../components/sections/NavbarNovaTemplate'
 import HeroTemplateNovo from '../components/sections/HeroTemplateNovo'
-import StepsNovoTemplate from '../components/sections/StepsNovoTemplate'
-import FaqNovoTemplate from '../components/sections/FaqNovoTemplate'
 
 // Lazy — abaixo da dobra
 const FeaturesNovaTemplate = lazy(
   () => import('../components/sections/FeaturesNovaTemplate'),
 )
-// const FeaturesSemImage = lazy(() =>
-//   import('../components/sections/FeaturesSemImage')
-// )
+
 const CtaNovoTemplate = lazy(
   () => import('../components/sections/CtaNovoTemplate'),
 )
@@ -20,11 +16,6 @@ const AboutNovoTemplate = lazy(
   () => import('../components/sections/AboutNovoTemplate'),
 )
 
-const TeamSectionNew = lazy(() => import('../components/sections/TeamSection'))
-
-const SocialMediaTemplate = lazy(
-  () => import('../components/sections/SocialMediaTemplate'),
-)
 const FooterNovoTemplate = lazy(
   () => import('../components/sections/FooterNovoTemplate'),
 )
@@ -34,12 +25,8 @@ const WhatsappAnimated = lazy(
 import { useContext } from 'react'
 import { ColorModeProvider } from '../context/UseContextArchive'
 import { useColorMode } from '../context/UseContextArchive'
-import Cards from '../components/sections/Cards'
-import InventarioComparativo from '../components/sections/Tabela'
+
 import Important from '../components/sections/Important'
-import Speed from '../components/sections/Speed'
-import Emergency from '../components/sections/Emergency'
-import { Diferences } from '../components/sections/Diferences'
 
 export default function Index() {
   const { colorMode, setColorMode } = useColorMode()
@@ -53,23 +40,27 @@ export default function Index() {
         <main>
           {/* LCP — NÃO usar lazy */}
           <HeroTemplateNovo colorMode={colorMode} obs={true} obsTwo={false} />
-          {/* Lazy sections */}
           <Suspense fallback={null}>
             <FeaturesNovaTemplate
               colorMode={colorMode}
               frasesDestaque={true}
               accordion={false}
             />
-            {/* <Speed colorMode={colorMode} /> */}
+          </Suspense>
+          <Suspense>
             <Important colorMode={colorMode} />
-
+          </Suspense>
+          <Suspense>
             <AboutNovoTemplate
               colorMode={colorMode}
               ButtonModal={false}
               benefits={false}
             />
+          </Suspense>
+          <Suspense>
             <CtaNovoTemplate colorMode={colorMode} />
-            {/* <StepsNovoTemplate colorMode={colorMode} /> */}
+          </Suspense>
+          <Suspense>
             <FooterNovoTemplate
               colorMode={colorMode}
               mapa={false}
@@ -82,12 +73,9 @@ export default function Index() {
               fraseFooter={true} // frase Google LLC
               obs={false}
             />
+          </Suspense>
+          <Suspense>
             <WhatsappAnimated colorMode={colorMode} />
-            {/* <Diferences colorMode={colorMode} /> */}
-            {/* <Cards colorMode={colorMode} /> */}
-            {/* <Emergency colorMode={colorMode} /> */}
-            {/* <SocialMediaTemplate colorMode={colorMode} /> */}
-            {/* <FaqNovoTemplate colorMode={colorMode} /> */}
           </Suspense>
         </main>
       </ColorModeProvider>
